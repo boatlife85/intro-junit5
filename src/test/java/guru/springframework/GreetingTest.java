904bba38d15e8df7a99a0f4efb0d50e8833cf0c7
Joshua S <35569800+boatlife85@users.noreply.github.com>
2020-03-29 22:38:12 -0400

    Played around with lifecycle annotations for JUnit, BeforeAll, BeforeEach, AfterEach, and AfterAll.  Visualization through println() statements showing in text the lifecycle annotations available to you.

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,21 +1,47 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
+    private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("Before - I am only called Once!!!");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each....");
+        greeting = new Greeting();
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
-    void testHelloWorld() {
-        Greeting greeting = new Greeting();
+    void HelloWorld1() {
         System.out.println(greeting.helloWorld("John"));
+    }
 
+    @Test
+    void HelloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        System.out.print("In After Each.......");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("After!!! ***** - I am only called Once!!!");
     }
 }
